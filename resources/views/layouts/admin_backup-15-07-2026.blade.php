63146536d2e0118768c3764baa98f6b0df7652de
jery-dot <jerryp.dotpitchtechnologies@gmail.com>
2026-07-14 22:02:05 +0200

    Updating the Admin Nav\Bar

--- resources/views/layouts/admin_backup-15-07-2026.blade.php
+++ resources/views/layouts/admin_backup-15-07-2026.blade.php
@@ -218,13 +218,13 @@
             </form>
 
             <div class="tb-acts">
-                {{-- <button class="ib" aria-label="Notifications">
+                <button class="ib" aria-label="Notifications">
                     <svg width="17" height="17" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
                         <path d="M18 8A6 6 0 0 0 6 8c0 7-3 9-3 9h18s-3-2-3-9"/>
                         <path d="M13.73 21a2 2 0 0 1-3.46 0"/>
                     </svg>
                     <span class="ndot"></span>
-                </button> --}}
+                </button>
 
                 {{-- 🔥 MODIFIÉ : Utilisation du guard admin --}}
                 <div class="tb-av2" title="{{ auth()->guard('admin')->user()->name ?? 'Admin' }}">
